c582d247a7996181076f0d9a2c7bd226b1eae32b
Summer <102731709+ckdgk7944@users.noreply.github.com>
2026-03-25 16:42:39 +0900

    [DnDn] plan-20260324155600-de25a7b6

--- variables.tf
+++ variables.tf
@@ -10,14 +10,12 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "public_subnet_cidr" {
-  description = "Public subnet CIDR"
-  type        = string
-  default     = "10.0.1.0/24"
-}
-
-variable "private_subnet_cidr" {
-  description = "Private subnet CIDR"
-  type        = string
-  default     = "10.0.2.0/24"
+variable "required_tags" {
+  description = "Required tags for all resources"
+  type        = map(string)
+  default     = {
+    Environment = "dev"
+    Team        = "platform"
+    Service     = "demo"
+  }
 }
